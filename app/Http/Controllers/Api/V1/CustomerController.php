72d31b854802f1d68aeaf853e897960e02f6f2fb
mero2online <mero2online@gmail.com>
2022-09-11 22:41:50 +0200

    ADD: Paginate to Customer controller index

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -18,7 +18,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return new CustomerCollection(Customer::all());
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
